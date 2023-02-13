e03d16c13c5baf5a837e050c507be7e890483620
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-02-14 00:27:54 +0100

    feat: add API fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 /* eslint-disable no-console */
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 // import { getWeatherDataFromLocation } from './api/weather';
 import './App.css';
 import Weather from './components/WeatherCard';
 import { useLocationWeather } from './hooks/useLocationWeather';
+import geoLocation from './mocks/location.json';
 
-// import handleGeolocation from './utils/location';
+import handleGeolocation from './utils/location';
 
 function App() {
   // const [weather, setWeather] = useState(locationWeather);
-  const weather = useLocationWeather();
-  // const [location, setLocation] = useState({});
+  const [location, setLocation] = useState(geoLocation);
+  const { weather, getWeather } = useLocationWeather(location);
 
   useEffect(() => {
-    // handleGeolocation(setLocation);
+    handleGeolocation(setLocation);
     // setWeather(locationWeather);
   }, []);
 
@@ -21,7 +22,8 @@ function App() {
     /* getWeatherDataFromLocation(location)
       .then((data) => setWeather(data))
       .catch((err) => console.error(err)); */
-  }, []);
+    getWeather();
+  }, [location]);
 
   return (
     <div className="App">
--- src/hooks/useLocationWeather.js
+++ src/hooks/useLocationWeather.js
@@ -1,28 +1,52 @@
-/* eslint-disable no-unsafe-optional-chaining */
-import APIWeather from '../mocks/weather-results.json';
+import { useState } from 'react';
+// import withResults from '../mocks/weather-results.json';
+import withNoResults from '../mocks/no-results.json';
 
-export const useLocationWeather = () => {
-  const locationWeather = APIWeather;
+const OPEN_WEATHER_API_KEY = 'd986ebd55d0d4652aa82e507c36c4419';
+const OPEN_WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
 
-  const weather = {
-    name: locationWeather?.name,
-    country: locationWeather?.sys.country,
-    date: new Date(locationWeather?.dt * 1000).toLocaleString(),
-    icon: locationWeather?.weather[0].icon,
-    description: locationWeather?.weather[0].description,
-    day_temp: Math.round(locationWeather?.main.temp_max),
-    night_temp: Math.round(locationWeather?.main.temp_min),
-    temperature: Math.round(locationWeather?.main.temp),
-    feels_like: Math.round(locationWeather?.main.feels_like),
-    rain: locationWeather?.rain?.['1h'],
-    wind: locationWeather?.wind?.speed,
-    humidity: locationWeather?.main.humidity,
-    visibility: locationWeather?.visibility / 1000,
-    clouds: locationWeather?.clouds.all,
-    snow: locationWeather?.snow?.['1h']
+export const useLocationWeather = (location) => {
+  const [APIweather, setAPIWeather] = useState({});
+
+  const locationWeather = APIweather;
+
+  const weather =
+    Object.keys(locationWeather).length === 0
+      ? {}
+      : {
+          name: locationWeather.name,
+          country: locationWeather.sys.country,
+          date: new Date(locationWeather.dt * 1000).toLocaleString(),
+          icon: locationWeather.weather[0].icon,
+          description: locationWeather.weather[0].description,
+          day_temp: Math.round(locationWeather.main.temp_max),
+          night_temp: Math.round(locationWeather.main.temp_min),
+          temperature: Math.round(locationWeather.main.temp),
+          feels_like: Math.round(locationWeather.main.feels_like),
+          rain: locationWeather.rain?.['1h'],
+          wind: locationWeather.wind?.speed,
+          humidity: locationWeather.main.humidity,
+          visibility: locationWeather.visibility / 1000,
+          clouds: locationWeather.clouds?.all,
+          snow: locationWeather.snow?.['1h']
+        };
+
+  const getWeather = () => {
+    if (Object.keys(location).length !== 0) {
+      // setAPIWeather(withResults);
+      const { lat, lon } = location;
+      fetch(
+        `${OPEN_WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
+      )
+        .then((res) => res.json())
+        .then((json) => setAPIWeather(json))
+        .catch((err) => console.error(err));
+    } else {
+      setAPIWeather(withNoResults);
+    }
   };
 
-  return weather;
+  return { weather, getWeather };
 };
 
 export default useLocationWeather;
